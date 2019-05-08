a282716bede380f286a2315a5f0cf966d4d3003f
Wolfik <Wolfik@DESKTOP-C3Q0MUB>
2019-05-09 00:39:53 +0200

    2opt done

--- ingest_poczty.py
+++ ingest_poczty.py
@@ -9,13 +9,16 @@ def get(filename):
         l = [make_tuple(p) for p in f.read().strip().splitlines()]
         #results = [[int(i[0]), int(i[1])] for i in l]
         return l
-    
+  
+def get_simple(filename):    
+    init = get(filename)
+    return [ [int(p[0]), round(float(p[2]),6)*1000000, round(float(p[3]),6)*1000000] for p in init]
   
 def get_dist_matrix(filename):
       
-    poczty = get("poczty.txt")
-    poczty_coords = [ [round(float(p[2]),6), round(float(p[3]),6)] for p in poczty]
-    indices = list(map(lambda x: x[0], poczty))
+    poczty = get(filename)
+    poczty_coords = [[ round(float(p[2]),6)*1000000, round(float(p[3]),6)*1000000] for p in poczty]
+    indices = list(map(lambda x: int(x[0]), poczty))
     df = pd.DataFrame(poczty_coords, columns=['lat', 'lon'], index=indices)
     dist_matrix = pd.DataFrame(distance_matrix(df.values, df.values), index=df.index, columns=df.index).to_numpy()
     return dist_matrix
--- nearestneighbor_runnable.py
+++ nearestneighbor_runnable.py
@@ -5,46 +5,59 @@ import sys
 import timeit
 import math
 
-import tspinput
+import ingest_poczty as ing
+
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
-data = tspinput.get(sys.argv[1])
 
 def closestpoint(point, route):
     dmin = float("inf")
     for p in route:
-        d = math.sqrt((int(point[0]) - int(p[0]))**2 + (int(point[1]) - int(p[1]))**2)
+        d = math.sqrt((int(point[1]) - int(p[1]))**2 + (int(point[2]) - int(p[2]))**2)
         if d < dmin:
             dmin = d
             closest = p
     return closest, dmin
 
-point, *route = data
-path = [point]
-sum = 0
-while len(route) >= 1:
-    closest, dist = closestpoint(path[-1], route)
+def nearestneighbor(data):
+    point, *route = data
+    path = [point]
+    sum = 0
+    while len(route) >= 1:
+        closest, dist = closestpoint(path[-1], route)
+        path.append(closest)
+        route.remove(closest)
+        sum += dist
+    # Go back the the beginning when done.
+    closest, dist = closestpoint(path[-1], [point])
     path.append(closest)
-    route.remove(closest)
     sum += dist
-# Go back the the beginning when done.
-closest, dist = closestpoint(path[-1], [point])
-path.append(closest)
-sum += dist
+    return path, sum
 
-print("Optimal route:", path)
-print("Length:", sum)
 
-A = np.array(path)
-PTS= np.array(data)
 
-plt.plot(*A.T)
-plt.scatter(*PTS.T,  s= 2, c ='m')
-# plt.plot(data[0], data[1])
-plt.show()
+    
+
+if __name__ == "__main__":
+    data = ing.get_simple("poczty.txt")
+
+    path, sum =  nearestneighbor(data)
+    
+    print("Optimal route:", path)
+    print("Length:", sum)
+    
+    A = np.array([ [p[1],p[2]] for p in path])
+    points_only = [ [p[1],p[2]] for p in data]
+    PTS= np.array(points_only)
+    
+    plt.plot(*A.T)
+    plt.scatter(*PTS.T,  s= 2, c ='m')
+
+    plt.show()   
+    
 
 
 
